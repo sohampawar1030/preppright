48d34e58f242f422c3d987b1aa59189b9698e043
Soham Pawar <sohampawar1030@gmail.com>
2026-05-09 20:18:52 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
+
+// Remove preloader after hydration
+const preloader = document.getElementById('preloader');
+if (preloader) {
+  setTimeout(() => {
+    preloader.style.opacity = '0';
+    setTimeout(() => preloader.remove(), 500);
+  }, 300);
+}
